ee78a74c8b97979141b1ae441e384ab61f79b85b
seifeslimene <s.slimene19@gmail.com>
2021-04-13 23:58:24 +0100

    second commit

---
+++ index.js
@@ -0,0 +1,7 @@
+// const { default: axios } = require('axios');
+
+// axios(
+//   'https://cdn.cookielaw.org/consent/23d76a88-8ebd-439c-a0dd-6da91b895891/23d76a88-8ebd-439c-a0dd-6da91b895891.json'
+// )
+//   .then((res) => console.log(res.data))
+//   .catch((err) => console.log(err));
